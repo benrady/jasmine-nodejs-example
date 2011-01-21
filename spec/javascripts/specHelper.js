4f7a8c8e25ef382441f232636af5fa7c72876e7d
Ben Rady <benrady@gmail.com>
2011-01-21 16:46:57 -0600

    Updated specHelper

--- spec/javascripts/specHelper.js
+++ spec/javascripts/specHelper.js
@@ -1,8 +1,27 @@
 var fs = require('fs');
-global.window = {};
-global.navigator = {};
-
+var jsdom = require("jsdom");
 require('util/underscore');
-require('application');
 
-_.extend(exports, global.app);
+beforeEach(function () {
+  $('body').empty()
+    .append('<div class="navbar"></div><div id="view-toolbar-controls" class="toolbar"> </div><div id="currentView"></div>');
+});
+
+window = jsdom.jsdom("<html><head></head><body></body></html>").createWindow();
+global.navigator = {
+  userAgent: 'jasmine'
+};
+global.window = window;
+global.document = window.document;
+global.location = "http://monitoring";
+require("jquery/jquery-1.4.2");
+if (window.$) { 
+  global.$ = window.$; 
+  global.jQuery = window.jQuery;
+}
+
+require('util/modit');
+require('util/moditTest');
+// Load your javascript here
+
+_.extend(exports, global.mon);
